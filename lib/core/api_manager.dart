d8ef60882c07059b13d9e7fe4c6534c3afbb7d48
RubanZ <ruban_1998@hotmail.com>
2021-12-12 20:58:00 +0300

    done Screen

--- lib/core/api_manager.dart
+++ lib/core/api_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
-
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 
 class APIManager {
@@ -51,198 +52,43 @@ class APIManager {
 
   Future<List<Map<String, String>>> rooms() async {
     return [
-        {
-            "id": "1",
-            "room_title": "313",
-            "seats": "50",
-            "square": "0"
-        },
-        {
-            "id": "2",
-            "room_title": "319",
-            "seats": "24",
-            "square": "0"
-        },
-        {
-            "id": "3",
-            "room_title": "320",
-            "seats": "10",
-            "square": "55.2"
-        },
-        {
-            "id": "4",
-            "room_title": "326",
-            "seats": "20",
-            "square": "44.5"
-        },
-        {
-            "id": "5",
-            "room_title": "340",
-            "seats": "40",
-            "square": "49.3"
-        },
-        {
-            "id": "6",
-            "room_title": "338",
-            "seats": "24",
-            "square": "38.2"
-        },
-        {
-            "id": "7",
-            "room_title": "339",
-            "seats": "30",
-            "square": "29.8"
-        },
-        {
-            "id": "8",
-            "room_title": "318",
-            "seats": "20",
-            "square": "44"
-        },
-        {
-            "id": "9",
-            "room_title": "308",
-            "seats": "20",
-            "square": "0"
-        },
-        {
-            "id": "10",
-            "room_title": "404",
-            "seats": "16",
-            "square": "17.8"
-        },
-        {
-            "id": "11",
-            "room_title": "337",
-            "seats": "к.кл",
-            "square": "36.3"
-        },
-        {
-            "id": "12",
-            "room_title": "442",
-            "seats": "к.кл",
-            "square": "28.9"
-        },
-        {
-            "id": "13",
-            "room_title": "407",
-            "seats": "100",
-            "square": "114.6"
-        },
-        {
-            "id": "14",
-            "room_title": "450",
-            "seats": "50",
-            "square": "69.5"
-        },
-        {
-            "id": "15",
-            "room_title": "458",
-            "seats": "50",
-            "square": "0"
-        },
-        {
-            "id": "16",
-            "room_title": "456",
-            "seats": "30",
-            "square": "0"
-        },
-        {
-            "id": "17",
-            "room_title": "453",
-            "seats": "50",
-            "square": "51.2"
-        },
-        {
-            "id": "19",
-            "room_title": "413",
-            "seats": "22",
-            "square": "40"
-        },
-        {
-            "id": "20",
-            "room_title": "414",
-            "seats": "18",
-            "square": "39.9"
-        },
-        {
-            "id": "22",
-            "room_title": "503",
-            "seats": "50",
-            "square": "76.4"
-        },
-        {
-            "id": "23",
-            "room_title": "522а",
-            "seats": "к.кл",
-            "square": ""
-        },
-        {
-            "id": "24",
-            "room_title": "536",
-            "seats": "30",
-            "square": "76.6"
-        },
-        {
-            "id": "25",
-            "room_title": "537",
-            "seats": "30",
-            "square": "34.1"
-        },
-        {
-            "id": "26",
-            "room_title": "537а",
-            "seats": "к.кл.",
-            "square": "0"
-        },
-        {
-            "id": "154",
-            "room_title": "каф.ВК",
-            "seats": "0",
-            "square": ""
-        },
-        {
-            "id": "155",
-            "room_title": "каф.ФВ",
-            "seats": "0",
-            "square": ""
-        },
-        {
-            "id": "1144",
-            "room_title": "Актовый зал",
-            "seats": "500",
-            "square": ""
-        },
-        {
-            "id": "1177",
-            "room_title": "417",
-            "seats": "20",
-            "square": "0"
-        },
-        {
-            "id": "1227",
-            "room_title": "Акаф",
-            "seats": "0",
-            "square": ""
-        },
-        {
-            "id": "1511",
-            "room_title": "435",
-            "seats": "30",
-            "square": "55.1"
-        },
-        {
-            "id": "1536",
-            "room_title": "517",
-            "seats": "20",
-            "square": "49.6"
-        },
-        {
-            "id": "1559",
-            "room_title": "215",
-            "seats": "20",
-            "square": "20"
-        }
-    ]
+      {"id": "1", "room_title": "313", "seats": "50", "square": "0"},
+      {"id": "2", "room_title": "319", "seats": "24", "square": "0"},
+      {"id": "3", "room_title": "320", "seats": "10", "square": "55.2"},
+      {"id": "4", "room_title": "326", "seats": "20", "square": "44.5"},
+      {"id": "5", "room_title": "340", "seats": "40", "square": "49.3"},
+      {"id": "6", "room_title": "338", "seats": "24", "square": "38.2"},
+      {"id": "7", "room_title": "339", "seats": "30", "square": "29.8"},
+      {"id": "8", "room_title": "318", "seats": "20", "square": "44"},
+      {"id": "9", "room_title": "308", "seats": "20", "square": "0"},
+      {"id": "10", "room_title": "404", "seats": "16", "square": "17.8"},
+      {"id": "11", "room_title": "337", "seats": "к.кл", "square": "36.3"},
+      {"id": "12", "room_title": "442", "seats": "к.кл", "square": "28.9"},
+      {"id": "13", "room_title": "407", "seats": "100", "square": "114.6"},
+      {"id": "14", "room_title": "450", "seats": "50", "square": "69.5"},
+      {"id": "15", "room_title": "458", "seats": "50", "square": "0"},
+      {"id": "16", "room_title": "456", "seats": "30", "square": "0"},
+      {"id": "17", "room_title": "453", "seats": "50", "square": "51.2"},
+      {"id": "19", "room_title": "413", "seats": "22", "square": "40"},
+      {"id": "20", "room_title": "414", "seats": "18", "square": "39.9"},
+      {"id": "22", "room_title": "503", "seats": "50", "square": "76.4"},
+      {"id": "23", "room_title": "522а", "seats": "к.кл", "square": ""},
+      {"id": "24", "room_title": "536", "seats": "30", "square": "76.6"},
+      {"id": "25", "room_title": "537", "seats": "30", "square": "34.1"},
+      {"id": "26", "room_title": "537а", "seats": "к.кл.", "square": "0"},
+      {"id": "154", "room_title": "каф.ВК", "seats": "0", "square": ""},
+      {"id": "155", "room_title": "каф.ФВ", "seats": "0", "square": ""},
+      {"id": "1144", "room_title": "Актовый зал", "seats": "500", "square": ""},
+      {"id": "1177", "room_title": "417", "seats": "20", "square": "0"},
+      {"id": "1227", "room_title": "Акаф", "seats": "0", "square": ""},
+      {"id": "1511", "room_title": "435", "seats": "30", "square": "55.1"},
+      {"id": "1536", "room_title": "517", "seats": "20", "square": "49.6"},
+      {"id": "1559", "room_title": "215", "seats": "20", "square": "20"}
+    ];
+  }
+
+  Future<void> createRequest(String buildId, String roomId, String title,
+      String about, List<XFile> files) async {
+    return;
   }
 }
--- lib/requests/add_request.dart
+++ lib/requests/add_request.dart
@@ -1,6 +1,7 @@
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:service_desk/core/api_manager.dart';
-import "dart:io";
+import 'dart:io';
 
 class RequestPage extends StatefulWidget {
   const RequestPage({Key? key}) : super(key: key);
@@ -13,24 +14,116 @@ class RequestPage extends StatefulWidget {
 
 class _MyRequestPageState extends State<RequestPage> {
   List<Map<String, String>> addressOptions = [];
-  String address = "1";
+  List<Map<String, String>> roomOptions = [];
+
+  List<XFile> imageFiles = [];
+
+  String? addressId;
+  String? roomId;
 
   final _titleController = TextEditingController();
   final _textController = TextEditingController();
 
-  Future<String> getBuildingData() async {
+  Future<void> getBuildingData() async {
     var res = await APIManager().building();
 
     setState(() {
       addressOptions = res;
     });
-    return "Sucess";
+  }
+
+  Future<void> getRoomData() async {
+    print(addressId);
+    var res = await APIManager().rooms();
+
+    setState(() {
+      roomOptions = res;
+    });
+  }
+
+  Future<void> _showChoiceImage(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Выбор источника"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: const Text("Галерея"),
+                    onTap: () {
+                      _openGallary(context);
+                    },
+                  ),
+                  const Padding(padding: EdgeInsets.all(8.0)),
+                  GestureDetector(
+                    child: const Text("Камера"),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   @override
   void initState() {
     super.initState();
     getBuildingData();
+  }
+
+  _openGallary(BuildContext context) async {
+    var images = await ImagePicker().pickMultiImage();
+
+    setState(() {
+      if (images != null) {
+        for (final image in images) {
+          imageFiles.add(image);
+        }
+      }
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async {
+    var image = await ImagePicker().pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (image != null) {
+        imageFiles.add(image);
+      }
+    });
+    Navigator.of(context).pop();
+  }
+
+  Widget _buildGridView() {
+    if (imageFiles.isNotEmpty) {
+      return Container(
+          height: 300,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 8, crossAxisSpacing: 8, crossAxisCount: 4),
+            itemBuilder: (ctx, index) => GestureDetector(
+              onTap: () {
+                setState(() {
+                  imageFiles.removeAt(index);
+                });
+              },
+              child: Image.file(
+                File(imageFiles[index].path),
+                fit: BoxFit.cover,
+              ),
+            ),
+            itemCount: imageFiles.length,
+          ));
+    }
+    return const Center(
+      child: Text("Нет фото"),
+    );
   }
 
   @override
@@ -42,17 +135,37 @@ class _MyRequestPageState extends State<RequestPage> {
         body: ListView(padding: const EdgeInsets.all(16), children: <Widget>[
           const SizedBox(height: 30),
           DropdownButton<String>(
-            value: address,
+            value: addressId,
             isExpanded: true,
+            hint: const Text('Корпус'),
             icon: const Icon(Icons.arrow_drop_down_outlined),
             onChanged: (newValue) {
               setState(() {
-                address = newValue!;
+                addressId = newValue!;
+                getRoomData();
               });
             },
             items: addressOptions.map((Map<String, String> item) {
               return DropdownMenuItem(
                 child: Text(item['adres'] as String),
+                value: item['id'].toString(),
+              );
+            }).toList(),
+          ),
+          const SizedBox(height: 30),
+          DropdownButton<String>(
+            value: roomId,
+            isExpanded: true,
+            hint: const Text('Аудитория'),
+            icon: const Icon(Icons.arrow_drop_down_outlined),
+            onChanged: (newValue) {
+              setState(() {
+                roomId = newValue!;
+              });
+            },
+            items: roomOptions.map((Map<String, String> item) {
+              return DropdownMenuItem(
+                child: Text(item['room_title'] as String),
                 value: item['id'].toString(),
               );
             }).toList(),
@@ -72,43 +185,24 @@ class _MyRequestPageState extends State<RequestPage> {
             decoration: const InputDecoration(hintText: 'Описание проблемы'),
           ),
           const SizedBox(height: 30),
+          ElevatedButton(
+              onPressed: () {
+                _showChoiceImage(context);
+              },
+              child: const Text("Добавить фото")),
+          // const Padding(padding: EdgeInsets.all(8.0)),
+          _buildGridView(),
+          ElevatedButton(
+              onPressed: () {
+                APIManager().createRequest(
+                    addressId as String,
+                    roomId as String,
+                    _titleController.text,
+                    _textController.text,
+                    imageFiles);
+                Navigator.of(context).pop();
+              },
+              child: const Text("Создать заявку")),
         ]));
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = 'One';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
   }
 }
